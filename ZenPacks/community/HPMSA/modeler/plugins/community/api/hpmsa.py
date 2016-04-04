5e13a1052510a3be165714c5843aa13ad4423d48
Temnikov Ilya <iatemnikov@gmail.com>
2016-04-04 14:47:26 +0500

    working modeler

--- ZenPacks/community/HPMSA/modeler/plugins/community/api/hpmsa.py
+++ ZenPacks/community/HPMSA/modeler/plugins/community/api/hpmsa.py
@@ -3,7 +3,12 @@ from Products.DataCollector.plugins.DataMaps import RelationshipMap, ObjectMap
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.web.client import getPage
 from pprint import pprint
-from ZenPacks.community.HPMSA.msaapi import msaapi
+from ZenPacks.community.HPMSA.msaapi import (
+    msaapi,
+    get_devicemap,
+    modeller_order,
+    )
+import pdb
 
 
 class hpmsa(PythonPlugin):
@@ -43,8 +48,7 @@ class hpmsa(PythonPlugin):
         protocol = 'https' if secure else 'http'
 
         api = msaapi(controllers, protocol, user, password, log)
-        self.device_map = api.get_devicemap()
-        self.modeller_order = api.get_modeller_order()
+
         results = {}
 
         if api.get_url() is None:
@@ -58,8 +62,9 @@ class hpmsa(PythonPlugin):
                 log.error("%s: %s", device.id, e)
 
             if xml:
-                results['product_version'] = api.get_msa_version(xml)
+                results['product-id'] = api.get_msa_version(xml)
 
+            self.device_map = get_devicemap()
             for componentclass in self.device_map:
                 cmd = self.device_map[componentclass]['xml_obj_command']
                 try:
@@ -70,27 +75,28 @@ class hpmsa(PythonPlugin):
                     results[componentclass] = api.get_relation(
                         xml, componentclass
                         )
-
+            # pprint(results)
             returnValue(results)
 
     def process(self, device, results, log):
         rm = []
         rm.append(ObjectMap({
-            'product_version': results['product_version']
+            'product-id': results['product-id']
         }))
-        for key in self.modeller_order:
-            for itemid in results[key]:
-                if itemid is not None:
+        for key in modeller_order:
+            m = results.get(key)
+            for r, c in m.items():
+                if r:
                     rm.append(RelationshipMap(
-                        compname=self.device_map[key]['compname']+itemid,
+                        compname=self.device_map[key]['compname']+r,
                         relname=self.device_map[key]['relname'],
                         modname=self.device_map[key]['modname'],
-                        objmaps=results[key][itemid],
+                        objmaps=c,
                         ))
                 else:
                     rm.append(RelationshipMap(
                         relname=self.device_map[key]['relname'],
                         modname=self.device_map[key]['modname'],
-                        objmaps=results[key][itemid],
+                        objmaps=c,
                         ))
         return rm
